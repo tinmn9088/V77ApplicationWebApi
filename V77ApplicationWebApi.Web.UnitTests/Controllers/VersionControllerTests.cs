7ba29ab19b0b233c14e038572b8529fa16176f85
tinmn9088@RO-SD22 <tinman.8.9@yandex.ru>
2024-10-03 14:42:37 +0300

    [Web] 100% Test Coverage

---
+++ V77ApplicationWebApi.Web.UnitTests/Controllers/VersionControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace V77ApplicationWebApi.Web.Controllers.UnitTests;
+
+public class VersionControllerTests
+{
+    private readonly VersionController _controller;
+
+    public VersionControllerTests() => _controller = new();
+
+    [Fact]
+    public void VersionController_GetAssemblyVersion_ShouldReturnNotNul()
+    {
+        // Act
+        string assemblyVersion = _controller.GetAssemblyVersion();
+
+        // Assert
+        Assert.NotNull(assemblyVersion);
+    }
+}
