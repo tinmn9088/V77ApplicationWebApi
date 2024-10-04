9fc8160f85f6d6ce1a69dfdca4b340615732de26
tinmn9088@RO-SD22 <tinman.8.9@yandex.ru>
2024-10-04 09:17:57 +0300

    [Core] Add `IV77ApplicationConnectionFactory`

---
+++ V77ApplicationWebApi.Core/IV77ApplicationConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace V77ApplicationWebApi.Core;
+
+public interface IV77ApplicationConnectionFactory
+{
+    ValueTask<IV77ApplicationConnection> GetConnectionAsync(ConnectionProperties properties, CancellationToken cancellationToken);
+}
