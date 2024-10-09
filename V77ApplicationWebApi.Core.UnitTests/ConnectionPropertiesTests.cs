42095a1ebbb56766f2c7ada04cc36845d6456341
tinmn9088@RO-SD22 <tinman.8.9@yandex.ru>
2024-10-09 10:35:14 +0300

    [Core] Add unit tests

---
+++ V77ApplicationWebApi.Core.UnitTests/ConnectionPropertiesTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xunit;
+
+namespace V77ApplicationWebApi.Core.UnitTests;
+
+public class ConnectionPropertiesTests
+{
+    private const string TestInfobasePath = @"D:\TestInfobase";
+
+    private const string TestUsername = "TestUsername";
+
+    private const string TestPassword = "TestPassword";
+
+    [Fact]
+    public void Equals_ShouldComparePropertiesValues()
+    {
+        // Arrange
+        ConnectionProperties properties1 = new(
+            infobasePath: TestInfobasePath,
+            username: TestUsername,
+            password: TestPassword);
+        ConnectionProperties properties2 = new(
+            infobasePath: TestInfobasePath,
+            username: TestUsername,
+            password: TestPassword);
+
+        // Assert
+        Assert.Equal(properties1, properties2);
+    }
+
+    [Fact]
+    public void ConnectionProperties_WhenInfobasePathIsNull_ThrowsArgumentNullException() =>
+        Assert.Throws<ArgumentNullException>(() => new ConnectionProperties(infobasePath: null, username: TestUsername, password: TestPassword));
+
+    [Fact]
+    public void ConnectionProperties_WhenTestUsernameIsNull_ThrowsArgumentNullException() =>
+        Assert.Throws<ArgumentNullException>(() => new ConnectionProperties(infobasePath: TestInfobasePath, username: null, password: TestPassword));
+
+    [Fact]
+    public void ConnectionProperties_WhenPasswordIsNull_ShouldCreate() =>
+        Assert.NotNull(() => new ConnectionProperties(infobasePath: TestInfobasePath, username: TestUsername, password: null));
+}
--- V77ApplicationWebApi.Core/ConnectionProperties.cs
+++ V77ApplicationWebApi.Core/ConnectionProperties.cs
@@ -4,11 +4,11 @@ namespace V77ApplicationWebApi.Core;
 
 public record ConnectionProperties
 {
-    public ConnectionProperties(string infobasePath, string username, string password, TimeSpan? initializeTimeout = null)
+    public ConnectionProperties(string infobasePath, string username, string? password, TimeSpan? initializeTimeout = null)
     {
         InfobasePath = infobasePath ?? throw new ArgumentNullException(nameof(infobasePath));
         Username = username ?? throw new ArgumentNullException(nameof(username));
-        Password = password ?? throw new ArgumentNullException(nameof(password));
+        Password = password;
         InitializeTimeout = initializeTimeout;
     }
 
@@ -16,7 +16,7 @@ public record ConnectionProperties
 
     public string Username { get; }
 
-    public string Password { get; }
+    public string? Password { get; }
 
     public TimeSpan? InitializeTimeout { get; }
 }
