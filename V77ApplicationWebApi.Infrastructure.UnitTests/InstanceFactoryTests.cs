6c058d564d8778a77792141178defc9a40b86080
tinmn9088@RO-SD22 <tinman.8.9@yandex.ru>
2024-10-09 09:21:42 +0300

    [Infrastructure] Increase code coverage

---
+++ V77ApplicationWebApi.Infrastructure.UnitTests/InstanceFactoryTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using V77ApplicationWebApi.Infrastructure.Exceptions;
+using Xunit;
+
+namespace V77ApplicationWebApi.Infrastructure.UnitTests;
+
+public class InstanceFactoryTests
+{
+    private readonly InstanceFactory _instanceFactory;
+
+    public InstanceFactoryTests() => _instanceFactory = new();
+
+    [Fact]
+    public void GetTypeFromProgID_ShouldReturnType()
+    {
+        Type expected = typeof(object);
+
+        Type actual = _instanceFactory.GetTypeFromProgID("System.Object");
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void GetTypeFromProgID_WhenProgIDIsInvalid_ThrowsFailedToGetTypeFromProgIDException() =>
+        Assert.Throws<FailedToGetTypeFromProgIDException>(() => _instanceFactory.GetTypeFromProgID("NonExistingProgID"));
+
+    [Fact]
+    public void CreateInstance_ShouldCreateInstance()
+    {
+        Type expectedType = typeof(object);
+
+        object instance = _instanceFactory.CreateInstance(expectedType);
+
+        Assert.IsType(expectedType, instance);
+    }
+
+    [Fact]
+    public void CreateInstance_WhenFailsToCreateInstance_ThrowsFailedToCreateInstanceException() =>
+        Assert.Throws<FailedToCreateInstanceException>(() => _instanceFactory.CreateInstance(typeof(ClassWithoutDefaultConstructor)));
+
+    private class ClassWithoutDefaultConstructor(object value)
+    {
+        public object Value { get; } = value;
+    }
+}
--- V77ApplicationWebApi.Infrastructure/IInstanceFactory.cs
+++ V77ApplicationWebApi.Infrastructure/IInstanceFactory.cs
@@ -14,7 +14,7 @@ internal interface IInstanceFactory
     Type GetTypeFromProgID(string progID);
 
     /// <summary>
-    /// Creates an instance of the specified type.
+    /// Creates an instance of the specified type using that type's default constructor.
     /// </summary>
     /// <param name="type">The type of object to create.</param>
     /// <returns>A reference to the newly created object.</returns>
